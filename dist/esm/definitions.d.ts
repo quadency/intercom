db2f514b272c9394e3c88b767d2303b9f12cfe1c
Andy Minkstein <andy@lamplightergames.com>
2022-05-31 21:55:47 -0400

    new build files

--- dist/esm/definitions.d.ts
+++ dist/esm/definitions.d.ts
@@ -1,15 +1,10 @@
-declare global {
-    interface PluginRegistry {
-        IntercomPlugin?: IntercomProtocol;
-    }
-}
-export interface IntercomProtocol {
+export interface IntercomPlugin {
     registerIdentifiedUser(options: {
         userId?: string;
         email?: string;
     }): Promise<void>;
     registerUnidentifiedUser(): Promise<void>;
-    updateUser(options: UserUpdateOptions): Promise<void>;
+    updateUser(options: IntercomUserUpdateOptions): Promise<void>;
     logout(): Promise<void>;
     logEvent(options: {
         name: string;
@@ -25,26 +20,37 @@ export interface IntercomProtocol {
     hideLauncher(): Promise<void>;
     displayInAppMessages(): Promise<void>;
     hideInAppMessages(): Promise<void>;
+    displayCarousel(options: {
+        carouselId: string;
+    }): Promise<void>;
     setUserHash(options: {
         hmac: string;
     }): Promise<void>;
     setBottomPadding(options: {
         value: string;
     }): Promise<void>;
-    /**
-     * Send push token to Intercom
-     *
-     * Only for Android
-     */
-    sendPushTokenToIntercom(options: RefreshedToken): Promise<void>;
-    /**
-     * Receive Intercom push notification
-     *
-     * Only for Android
-     */
-    receivePush(notification: PushNotificationSchema): Promise<void>;
+    sendPushTokenToIntercom(options: {
+        value: string;
+    }): Promise<void>;
+    receivePush(notification: IntercomPushNotificationData): Promise<void>;
 }
-export interface UserUpdateOptions {
+export interface IntercomPushNotificationData {
+    conversation_id: string;
+    message: string;
+    body: string;
+    author_name: string;
+    image_url: string;
+    app_name: string;
+    receiver: string;
+    conversation_part_type: string;
+    intercom_push_type: string;
+    uri: string;
+    push_only_conversation_id: string;
+    instance_id: string;
+    title: string;
+    priority: number;
+}
+export interface IntercomUserUpdateOptions {
     userId?: string;
     email?: string;
     name?: string;
